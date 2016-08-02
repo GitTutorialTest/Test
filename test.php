0124b0ac8c9990b79e767a4615ded937c8ce973c
t0003 <Test@example.com>
2016-08-02 15:42:34 +0200

    Übung

---
+++ test.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $TestArray = array('a' => 1, 'b' => 3 );
+      print_r($TestArray);
+     ?>
+     <a href="d
+     d
+     df
+     f
+     da
+     sf
+     ds
+     fds
+     f
+     dsf
+     aa
+
+     a
+     "></a>
+  </body>
+</html>
